95a0a15bad290c19b28795cb123c848fe35de925
guranytou <guranytou@gmail.com>
2022-06-17 14:38:21 +0900

    Cloud9作成

--- cloud9.tf
+++ cloud9.tf
@@ -1,6 +1,6 @@
 resource "aws_cloud9_environment_ec2" "management" {
   instance_type               = "t2.micro"
   name                        = "sbcntr-dev"
-  subnet_id = aws_subnet.sbcntr_subnet_pub_manage_1c.id
+  subnet_id = aws_subnet.sbcntr_subnet_pub_manage_1a.id
 }
 
