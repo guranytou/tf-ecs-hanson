a6c85b37b42064fd25e2901ef9626388335591d2
guranytou <guranytou@gmail.com>
2022-06-17 18:25:57 +0900

    AWSサービス用のVPCE作成

--- network.tf
+++ network.tf
@@ -176,6 +176,23 @@ resource "aws_route_table_association" "db_1c" {
 }
 
 ####################################################
-# Private Link/ECR
+# Private Link/AWS Service
 ####################################################
 
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.sbcntr_vpc.id
+  service_name      = "com.amazonaws.ap-northeast-1.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.sbcntr_vpc.id
+  service_name      = "com.amazonaws.ap-northeast-1.ecr.api"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.sbcntr_vpc.id
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+}
