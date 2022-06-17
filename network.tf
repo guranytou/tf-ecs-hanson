586c9b29f89ee34d69334415f639fd057ecbe9e9
guranytou <guranytou@gmail.com>
2022-06-17 18:34:11 +0900

    PrivateDNS機能を有効にする&タグ付与

--- network.tf
+++ network.tf
@@ -180,19 +180,33 @@ resource "aws_route_table_association" "db_1c" {
 ####################################################
 
 resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id            = aws_vpc.sbcntr_vpc.id
-  service_name      = "com.amazonaws.ap-northeast-1.ecr.dkr"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = aws_vpc.sbcntr_vpc.id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  tags = {
+    Name = "sbcntr-vpce-ecr-dkr"
+  }
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id            = aws_vpc.sbcntr_vpc.id
-  service_name      = "com.amazonaws.ap-northeast-1.ecr.api"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = aws_vpc.sbcntr_vpc.id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  tags = {
+    Name = "sbcntr-vpce-ecr-api"
+  }
 }
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.sbcntr_vpc.id
   service_name      = "com.amazonaws.ap-northeast-1.s3"
   vpc_endpoint_type = "Gateway"
+
+  tags = {
+    Name = "sbcntr-vpce-s3"
+  }
 }
