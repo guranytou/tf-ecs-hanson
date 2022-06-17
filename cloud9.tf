8e04e4430fb26ea7312fe26211911e0e5962fe2e
guranytou <guranytou@gmail.com>
2022-06-17 22:56:34 +0900

    applyし直し

--- cloud9.tf
+++ cloud9.tf
@@ -1,3 +1,6 @@
+############################################
+# Cloud9
+############################################
 resource "aws_cloud9_environment_ec2" "management" {
   instance_type               = "t2.micro"
   name                        = "sbcntr-dev"
@@ -6,3 +9,14 @@ resource "aws_cloud9_environment_ec2" "management" {
   image_id                    = "amazonlinux-2-x86_64"
 }
 
+############################################
+# EC2 for Cloud9
+############################################
+# resource "aws_instance" "cloud9" {
+#   ami = "ami-011cfcf7a08034ef3"
+#   instance_type = "t2.micro"
+# }
+
+############################################
+# IAM for Cloud9
+############################################
