a39faece412505551608fec9b48ff1f80644d912
guranytou <guranytou@gmail.com>
2022-06-17 18:02:39 +0900

    ECRリポジトリを作成

--- container.tf
+++ container.tf
@@ -2,7 +2,16 @@
 # ECR
 ####################################################
 
-# resource "aws_ecr_repository" "backend" {
-#   name = "sbcntr-backend"
+resource "aws_ecr_repository" "backend" {
+  name = "sbcntr-backend"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+}
 
-# }
+resource "aws_ecr_repository" "frontenc" {
+  name = "sbcntr-frontend"
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+}
