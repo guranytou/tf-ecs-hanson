d0be3e396c03dc724718b95ed97ce2a56db15441
guranytou <guranytou@gmail.com>
2022-06-17 14:40:22 +0900

    Cloud9作成

--- cloud9.tf
+++ cloud9.tf
@@ -1,6 +1,7 @@
 resource "aws_cloud9_environment_ec2" "management" {
   instance_type               = "t2.micro"
   name                        = "sbcntr-dev"
+  automatic_stop_time_minutes = 30
   subnet_id = aws_subnet.sbcntr_subnet_pub_manage_1a.id
 }
 
